668a0fd89d3b699a2440534cf2f67a4704317713
PrepaCivile <mathieumontaroux@gmail.com>
2026-08-12 19:34:36 -0400

    fix: dynamicParams false pour pages documents

--- coro-website/app/documents/[doc]/page.tsx
+++ coro-website/app/documents/[doc]/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
+export const dynamicParams = false;
+
 export function generateStaticParams() {
   return [
     { doc: 'plan-mesures-urgence-pmu' },
